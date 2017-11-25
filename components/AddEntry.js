8e38bae5f30da8074457e47cc60794dfc1e711d5
Suveshtr <suvesh.trivedi@gmail.com>
2017-11-25 10:59:16 +0530

    Add step and slide updaers

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,47 @@ import { View, Text } from 'react-native'
 import { getMetricMetaInfo } from '../util/helpers'
 
 export default class AddEntry extends React.Component {
+  //steps initialized to 0
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep:0,
+    eat:0
+  }
+
+// for run, bike and swim metric, increment the state
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState( (state) => {
+      const count = state[metric] + step
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    
+    this.setState( (state) => {
+      const count = state[metric] - getMetricMetaInfo(metric).step
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count
+      }
+    })
+  }
+
+  slide = (metric, value) => {
+     this.setState( () => ({
+      [metric]: value,
+    }))
+  }
+
   render() {
     return (
       <View>
