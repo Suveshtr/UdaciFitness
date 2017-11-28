2c95fc81209737bd9775c71d355aef8015eeca0d
Suveshtr <suvesh.trivedi@gmail.com>
2017-11-28 12:30:22 +0530

    Update the title in navigation option dynamically

--- components/EntryDetails.js
+++ components/EntryDetails.js
@@ -2,6 +2,19 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 class EntryDetails extends React.Component {
+
+  static navigationOptions = ({navigation}) => {
+    const { entryId } = navigation.state.params
+    const year = entryId.slice(0,4)
+    const month = entryId.slice(5,7)
+    const day = entryId.slice(8)
+
+    return {
+      title: `${month}/${day}/ ${year}`
+    }
+  }
+
+
   render () {
     return (
       <View>
